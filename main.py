113f26d7dfaa7e41ac9c4f635caa83d7ed37ee6f
Till Hoffmann <tillahoffmann@gmail.com>
2014-12-16 16:52:04 +0000

    Modified simulation loop.

--- main.py
+++ main.py
@@ -1,38 +1,54 @@
 from simulation import *
 import matplotlib.pyplot as plt
 
-# Define a number of nodes and simulation steps
-num_nodes = 100
-num_steps = 100000
-np.random.seed(42)
-concentration = 3
+def main_simulated(stationarity):
 
-# Create a graph
-graph = nx.erdos_renyi_graph(num_nodes, 5 / float(num_nodes))
-# Remove any isolated nodes and relabel the nodes
-graph = remove_isolates(graph)
-# Obtain the number of remaining nodes and initialise the alpha and beta vectors
-num_nodes = graph.number_of_nodes()
-alpha = concentration * np.ones(num_nodes)
-beta = concentration * np.ones(num_nodes)
+    # Define a number of nodes and simulation steps
+    num_nodes = 100
+    num_steps = 50000
+    seed=42
+    np.random.seed(seed)
+    concentration = 1
 
-# Run the simulation
-alphas, betas = simulate(graph, alpha, beta, num_steps, stationarity='moran')
+    # Create a graph
+    graph = nx.erdos_renyi_graph(num_nodes, 5 / float(num_nodes), seed)
+    # Remove any isolated nodes and relabel the nodes
+    graph = remove_isolates(graph)
+    # Obtain the number of remaining nodes and initialise the alpha and beta vectors
+    num_nodes = graph.number_of_nodes()
+    alpha = concentration * np.ones(num_nodes)
+    beta = concentration * np.ones(num_nodes)
 
-# Compute the fraction of `alpha` balls in the population and visualise
-probability = np.mean(alphas / (alphas + betas), axis=1)
-plt.figure()
-plt.plot(probability)
-plt.xlabel('Step number')
-plt.ylabel('Population probability')
-plt.tight_layout()
+    # Run the simulation
+    return simulate(graph, alpha, beta, num_steps, stationarity=stationarity)
 
-# Plot the number of blue and red balls as a function of time
-plt.figure()
-plt.plot(np.sum(alphas, axis=1), color='b')
-plt.plot(np.sum(betas, axis=1), color='r')
-plt.xlabel('Step number')
-plt.ylabel('Number of balls')
-plt.tight_layout()
+def plot_stats(name, data):
+    alphas, betas = data
+    stats = SummaryStats(alphas, betas)
+    stats.collect_stats()
 
-plt.show()
+    # Compute the fraction of `alpha` balls in the population and visualise
+    probability = np.mean(alphas / (alphas + betas), axis=1)
+    plt.figure()
+    plt.plot(probability)
+    plt.xlabel('Step number')
+    plt.ylabel('Population probability')
+    plt.tight_layout()
+
+    # Plot the number of blue and red balls as a function of time
+    plt.figure()
+    plt.plot(np.sum(alphas, axis=1), color='b')
+    plt.plot(np.sum(betas, axis=1), color='r')
+    plt.xlabel('Step number')
+    plt.ylabel('Number of balls')
+    plt.tight_layout()
+
+    plt.figure()
+    plt.plot(stats.stats["mean_entropy_per_urn"])
+    plt.xlabel('Step number')
+    plt.ylabel('Mean entropy')
+
+    plt.show()
+
+plot_stats('moran', main_simulated('moran'))
+plot_stats('polya', main_simulated('polya'))
