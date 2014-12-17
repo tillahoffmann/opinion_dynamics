e1046d662f333eed29677cfeacf59efa72bfd6c2
Ouldo <t.ouldridge@imperial.ac.uk>
2014-12-17 17:29:36 +0000

    Tom Os little program

--- TomO.py
+++ TomO.py
@@ -33,33 +33,54 @@ def statistic_num_f1(balls):
 	num = (balls[1,0]+balls[1,1])
     	return (num)
 
-np.random.seed(70)
-num_steps = 100000
-num_nodes=2
-G = nx.star_graph(num_nodes-1)
-G = G.to_directed()
-balls = np.ones((num_nodes, 2))
+def _main():
 
-steps = simulate(G, balls, num_steps)
-belief_f0 = evaluate_statistic(balls, steps, statistic_belief_f0)
-belief_f1 = evaluate_statistic(balls, steps, statistic_belief_f1)
+	"set up a graph"
+	np.random.seed(50)
+	num_steps = 10
+	num_nodes=2
+	G = nx.star_graph(num_nodes-1)
+	G=G.to_directed()
+	balls = np.ones((num_nodes, 2))	
 
-num_f0 = evaluate_statistic(balls, steps, statistic_num_f0)
-num_f1 = evaluate_statistic(balls, steps, statistic_num_f1)
+	"I set the balls of one type to 0." 	
+	balls[0,0]=0
+	balls[1,0]=0
 
-plt.plot(num_f0,num_f1)   
-plt.xlabel('num f0')
-plt.ylabel('num f1')
-plt.tight_layout()
-plt.show()
+	print[balls]
 
-plt.plot(belief_f0)
-plt.plot(belief_f1)
-plt.plot(belief_f1+belief_f0)
-plt.xlabel('Time step')
-plt.ylabel('Belief of f0, f1')
-plt.tight_layout()
-plt.show()
+	steps = simulate(G, balls, num_steps)
 
 
+	"I ask for the belief and the total number of balls in each urn as a function of time."
+	belief_f0 = evaluate_statistic(balls, steps, statistic_belief_f0)
+	belief_f1 = evaluate_statistic(balls, steps, statistic_belief_f1)
 
+	num_f0 = evaluate_statistic(balls, steps, statistic_num_f0)
+	num_f1 = evaluate_statistic(balls, steps, statistic_num_f1)
+
+	"output results - as you can see,balls of the type that should be absent are geerated."
+	print(steps)
+	print(num_f0)
+	print(num_f1)
+
+	print(belief_f0)
+	print(belief_f1)
+
+	plt.plot(num_f0,num_f1)   
+	plt.xlabel('num f0')
+	plt.ylabel('num f1')
+	plt.tight_layout()
+	plt.show()
+
+	plt.plot(belief_f0)
+	plt.plot(belief_f1)
+	plt.plot(belief_f1+belief_f0)
+	plt.xlabel('Time step')
+	plt.ylabel('Belief of f0, f1')
+	plt.tight_layout()
+	plt.show()
+
+
+if __name__=='__main__':
+    _main()
