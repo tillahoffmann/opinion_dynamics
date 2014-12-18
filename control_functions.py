34368946d5f35ecd9a11f152c7f6f3bbe87bad62
Sumeet Agarwal <sumeet@iitd.ac.in>
2014-12-18 17:47:57 +0000

    Modified control_functions.py to add burn_out parameter and other features.

--- control_functions.py
+++ control_functions.py
@@ -2,23 +2,23 @@ import numpy as np
 import networkx as nx
 from random import randint
 
-
-def hub_control(graph, balls, step, burn_in=5000, control_interval=100, control_fraction=0.1, control_balls=10):
+def hub_control(graph, balls, step, burn_in=5000, burn_out=10000, control_interval=100, control_fraction=0.1, control_balls_fraction=0.1):
     """
     Exercises control by periodically targeting hubs (high-degree nodes) and sending them a fixed number of balls of colour 0.
     :param graph: The graph on which dynamics are being simulated.
     :param balls: The current distribution of balls at nodes.
     :param step: The current time step.
     :param burn_in: The number of time steps to wait for before exercising any control.
+    :param burn_out: The number of time steps after which to stop exercising control.
     :param control_interval: The interval in time steps at which control is to be exercised.
     :param control_fraction: The fraction of nodes to be targeted.
-    :param control_balls: The total number of balls available to the controller at each iteration.
+    :param control_balls_fraction: The total number of balls available to the controller at each iteration, as a fraction of the total number of nodes in the graph.
     """	
     # Array to store control changes
     controls = []	
  
     # Check if anything is to be done at this step
-    if step >= burn_in and step % control_interval == 0:
+    if step >= burn_in and step < burn_out and step % control_interval == 0:
 
     	num_nodes = graph.number_of_nodes()
     	degree = np.asarray(graph.out_degree().values())
@@ -27,7 +27,7 @@ def hub_control(graph, balls, step, burn_in=5000, control_interval=100, control_
     	num_hubs = int(num_nodes*control_fraction)
     	top_hubs = degree.argsort()[-num_hubs:][::-1]
 
-        balls_per_node = float(control_balls) / num_hubs
+        balls_per_node = num_nodes*control_balls_fraction / num_hubs
 	for hub in range(num_hubs):
             # Send some colour 0 balls to this hub
             controls.append((top_hubs[hub], 0, balls_per_node))
@@ -35,27 +35,66 @@ def hub_control(graph, balls, step, burn_in=5000, control_interval=100, control_
     return controls
 
 
-def random_control(graph, balls, step, burn_in=5000, control_interval=100, control_fraction=0.1, control_balls=10):
+def degree_control(graph, balls, step, burn_in=5000, burn_out=10000, control_interval=100, control_fraction=0.1, control_balls_fraction=0.1):
+    """
+    Exercises control by periodically targeting a certain fraction of nodes (with higher-degree nodes more likely to be chosen) and sending them a fixed number of balls of colour 0.
+    :param graph: The graph on which dynamics are being simulated.
+    :param balls: The current distribution of balls at nodes.
+    :param step: The current time step.
+    :param burn_in: The number of time steps to wait for before exercising any control.
+    :param burn_out: The number of time steps after which to stop exercising control.
+    :param control_interval: The interval in time steps at which control is to be exercised.
+    :param control_fraction: The fraction of nodes to be targeted.
+    :param control_balls_fraction: The total number of balls available to the controller at each iteration, as a fraction of the total number of nodes in the graph.
+    """	
+    # Array to store control changes
+    controls = []	
+ 
+    # Check if anything is to be done at this step
+    if step >= burn_in and step < burn_out and step % control_interval == 0:
+
+    	num_nodes = graph.number_of_nodes()
+    	cumulative_degree = np.cumsum(np.asarray(graph.out_degree().values()))
+	
+    	# Get indices of nodes to be targeted
+    	num_targets = int(num_nodes*control_fraction)
+	
+        balls_per_node = num_nodes*control_balls_fraction / num_targets
+	for node in range(num_targets):
+	    # Select index of node to target	
+	    rand = randint(1,cumulative_degree[num_nodes-1])
+	    node = 0
+	    while cumulative_degree[node] < rand:
+		node = node + 1
+
+            # Send some colour 0 balls to this node
+            controls.append((node, 0, balls_per_node))
+
+    return controls
+
+
+def random_control(graph, balls, step, burn_in=5000, burn_out=10000, control_interval=100, control_fraction=0.1, control_balls_fraction=0.1):
     """
     Exercises control by periodically targeting a certain fraction of the nodes at random and sending them a fixed number of balls of colour 0.
     :param graph: The graph on which dynamics are being simulated.
     :param balls: The current distribution of balls at nodes.
     :param step: The current time step.
     :param burn_in: The number of time steps to wait for before exercising any control.
+    :param burn_out: The number of time steps after which to stop exercising control.
     :param control_interval: The interval in time steps at which control is to be exercised.
     :param control_fraction: The fraction of nodes to be targeted.
-    :param control_balls: The total number of balls available to the controller at each iteration.
+    :param control_balls_fraction: The total number of balls available to the controller at each iteration, as a fraction of the total number of nodes in the graph.
     """
     # Array to store control changes
     controls = []	
 
     # Check if anything is to be done at this step
-    if step >= burn_in and step % control_interval == 0:
+    if step >= burn_in and step < burn_out and step % control_interval == 0:
 
 	num_nodes = graph.number_of_nodes()
     	num_targets = int(num_nodes*control_fraction)
 
-        balls_per_node = float(control_balls)/num_targets
+        balls_per_node = num_nodes*control_balls_fraction/num_targets
         for node in range(num_targets):
             # Send some colour 0 balls to an arbitrarily chosen node
             controls.append((int(num_nodes*node/num_targets), 0, balls_per_node))
@@ -63,21 +102,22 @@ def random_control(graph, balls, step, burn_in=5000, control_interval=100, contr
     return controls
 
 
-def tom_control(graph, balls, step, burn_in=5000, control_interval=100, control_fraction=0.1, control_balls=10):
+def tom_control(graph, balls, step, burn_in=5000, burn_out=10000, control_interval=100, control_fraction=0.1, control_balls_fraction=0.1):
     """
         Exercises control by periodically targeting a certain fraction of the nodes at random and sending them a fixed number of balls of colour 0.
         :param graph: The graph on which dynamics are being simulated.
         :param balls: The current distribution of balls at nodes.
         :param step: The current time step.
     	:param burn_in: The number of time steps to wait for before exercising any control.
+	:param burn_out: The number of time steps after which to stop exercising control.
         :param control_interval: The interval in time steps at which control is to be exercised.
         :param control_fraction: The fraction of nodes to be targeted.
-        :param control_balls: The total number of balls available to the controller at each iteration.
+        :param control_balls_fraction: The total number of balls available to the controller at each iteration, as a fraction of the total number of nodes in the graph.
     """
     # Array to store control changes
     controls = []
 
-    if step >= burn_in and step % control_interval == 0:
+    if step >= burn_in and step < burn_out and step % control_interval == 0:
 	influence = np.asarray([0] * graph.number_of_nodes())
     	for node in graph.nodes():
         	neighbors_array = graph.neighbors(node)
@@ -86,10 +126,11 @@ def tom_control(graph, balls, step, burn_in=5000, control_interval=100, control_
             if balls[neighbors, 0] < balls[neighbors, 1]:
                 influence[node] += 1
 
-    	num_influence = int(graph.number_of_nodes() * control_fraction)
+	num_nodes = graph.number_of_nodes()
+    	num_influence = int(num_nodes * control_fraction)
     	top_influence_nodes = influence.argsort()[-num_influence:][::-1]
 
-        balls_per_node = control_balls / num_influence
+        balls_per_node = num_nodes*control_balls_fraction / num_influence
 
         for i in range(num_influence):
             # Add balls of colour 0
@@ -98,25 +139,26 @@ def tom_control(graph, balls, step, burn_in=5000, control_interval=100, control_
     return controls
 
 
-def broadcast_control(graph, balls, step, burn_in=5000,  control_interval=100, control_balls=10):
+def broadcast_control(graph, balls, step, burn_in=5000, burn_out=10000, control_interval=100, control_balls_fraction=0.1):
     """
     Exercises control by periodically sending all nodes a fixed number of balls of colour 0.
     :param graph: The graph on which dynamics are being simulated.
     :param balls: The current distribution of balls at nodes.
     :param step: The current time step.
     :param burn_in: The number of time steps to wait for before exercising any control.
+    :param burn_out: The number of time steps after which to stop exercising control.
     :param control_interval: The interval in time steps at which control is to be exercised.
-    :param control_balls: The total number of balls available to the controller at each iteration.
+    :param control_balls_fraction: The total number of balls available to the controller at each iteration, as a fraction of the total number of nodes in the graph.
     """
     # Array to store control changes
     controls = []	
 
     # Check if anything is to be done at this step
-    if step >= burn_in and step % control_interval == 0: 
+    if step >= burn_in and step < burn_out and step % control_interval == 0: 
 
 	num_nodes = graph.number_of_nodes()
     
-        balls_per_node = float(control_balls) / num_nodes
+        balls_per_node = num_nodes*control_balls_fraction / num_nodes
 
         for node in range(num_nodes):
             # Send some colour 0 balls to this node
