1e89dd3c6607a784d3223f6326569fb408ae3922
Sumeet Agarwal <sumeet@iitd.ac.in>
2014-12-19 16:00:26 +0000

    Added young_control strategy.

--- control_functions.py
+++ control_functions.py
@@ -165,3 +165,37 @@ def broadcast_control(graph, balls, step, burn_in=5000, burn_out=10000, control_
             controls.append((node, 0, balls_per_node))
 
     return controls
+
+
+def young_control(graph, balls, step, burn_in=5000, burn_out=10000, control_interval=1000, control_fraction=0.1, control_balls_fraction=1):
+    """
+    Exercises control by periodically targeting nodes with few balls and sending them a fixed number of balls of colour 0.
+    :param graph: The graph on which dynamics are being simulated.
+    :param balls: The current distribution of balls at nodes.
+    :param step: The current time step.
+    :param burn_in: The number of time steps to wait for before exercising any control.
+    :param burn_out: The number of time steps after which to stop exercising control.
+    :param control_interval: The interval in time steps at which control is to be exercised.
+    :param control_fraction: The fraction of nodes to be targeted.
+    :param control_balls_fraction: The total number of balls available to the controller at each iteration, as a fraction of the total number of nodes in the graph.
+    """	
+    # Array to store control changes
+    controls = []	
+ 
+    # Check if anything is to be done at this step
+    if step >= burn_in and step < burn_out and step % control_interval == 0:
+
+    	num_nodes = graph.number_of_nodes()
+	for i in range(num_nodes):
+    		experience[i] = balls[i,0] + balls[i,1]
+
+    	# Get indices of youngest nodes
+    	num_targets = int(num_nodes*control_fraction)
+    	youngest = experience.argsort()[:num_targets][::1]
+
+        balls_per_node = float(num_nodes*control_balls_fraction) / num_targets
+	for node in range(num_targets):
+            # Send some colour 0 balls to this hub
+            controls.append((youngest[node], 0, balls_per_node))
+
+    return controls
